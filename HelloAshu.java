5fb4c14e21aa64a4530261ff52aad1cc7d7a461f
Shubhraaaj <ashu.shubhraj@gmail.com>
2017-08-25 22:54:04 +0530

    "Added Exclamation mark at the end"

--- HelloAshu.java
+++ HelloAshu.java
@@ -3,6 +3,6 @@ public class HelloAshu
 	public static void main(String args[])
 	{
 		System.out.println("Hello Ashu");
-		System.out.println("Entreprenuer");
+		System.out.println("Entreprenuer!");
 	}
 }
