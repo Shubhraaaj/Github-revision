e8b6414f435a32a07bab7f3c1f4ae93488b2492f
Shubhraaaj <ashu.shubhraj@gmail.com>
2017-08-25 18:10:33 +0530

    Added a loop to Helloworld

--- Helloworld.java
+++ Helloworld.java
@@ -3,5 +3,7 @@ public class Helloworld
 	public static void main(String args[])
 	{
 		System.out.println("Hello Shubhraj Prasad Singh");
+		for(int i=1;i<10;i++)
+			System.out.println("ASHU");
 	}
 }
